efd0bca2c7abec9395127bcdc049d8415114f726
Zerone Vu <zeronevu@gmail.com>
2019-09-16 23:26:13 +0700

    create docker for nodejs 11 with vue/cli in mostsimple form

--- 9mareonjava/entrypoint.sh
+++ 9mareonjava/entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Some command(s) has been passed to container? Execute them and exit.
 export NODE_PATH=`npm root -g 2> /dev/null`
 if [ -z "$cmd" ]; then
-    if [[ ! $@ ]]; then
+    if [ ! $@ ]; then
         echo "error: please pass a file to run." 1>&2
         exit 1
     fi
